da5eb164c78ca2b0aa1700b00110dcb5531dbbb4
Victor Kipkurui <kipkurui.victor18@students.dkut.ac.ke>
2024-08-29 22:42:46 +0300

    created login and signup pages

--- loginsignup/base/urls.py
+++ loginsignup/base/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path, include
+from django.urls import path 
+from . import views
 
 urlpatterns = [
-    path('', include('base.urls'))
+    path("", views.register, name='register'),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name='logout'),
+    path("home/", views.home, name='home')
 ]
